397d88cb3e43575d9b7819447d50de59bfa52716
eduRamos1620 <eduardoramosrosales16@gmail.com>
2025-01-23 14:45:16 -0600

    Se agrega uso de PathVariable

---
+++ src/main/java/com/ramos/webapp/springboot_web/controllers/PathVariableController.java
@@ -0,0 +1,33 @@
+package com.ramos.webapp.springboot_web.controllers;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ramos.webapp.springboot_web.models.DTO.ParamDto;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+@RequestMapping("/api/var")
+public class PathVariableController {
+    @GetMapping("/baz/{message}")
+    public ParamDto baz(@PathVariable String message){
+        ParamDto param = new ParamDto();
+        param.setMessage(message);
+        return param;
+    }
+
+    @GetMapping("/mix/{product}/{id}")
+    public Map<String, Object> mixPathVar(@PathVariable String product, @PathVariable Long id) {
+        Map<String, Object> json = new HashMap<>();
+        json.put("product", product);
+        json.put("id", id);
+        return json;
+    }
+    
+}
